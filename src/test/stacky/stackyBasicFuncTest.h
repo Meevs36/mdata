f0c5f2602bcd4088cfe211dd4f48b413aef301f6
Meevs <levi.meston360@gmail.com>
2021-12-05 20:43:06 +0000

    Added function headers to all tests in the 'stackBasicFuncTest.h' file. This addes a little bit more description as to waht is expected out of the corresponding test.

--- src/test/stacky/stackyBasicFuncTest.h
+++ src/test/stacky/stackyBasicFuncTest.h
@@ -10,6 +10,15 @@
 
 #include <string.h>
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:31:15 2021
+ * Function Name -- stackyInitTest 
+ * Function Purpose -- Tests the initialization of a Stacky stack, asserting that the data entered into the stack is properly stored.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (stackyInitTest)
 {
   stacky *stack = NULL;
@@ -22,6 +31,15 @@ START_TEST (stackyInitTest)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:31:54 2021
+ * Function Name -- simpleStack 
+ * Function Purpose -- Tests that the push and pop commands for a Stacky stack are properly working as intended.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (simpleStack)
 {
   stacky *stack = NULL;
@@ -34,6 +52,64 @@ START_TEST (simpleStack)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:41:01 2021
+ * Function Name -- popTest 
+ * Function Purpose -- Tests the functionality of the Stacky module to properly handle the popping of elements off of it's stack.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
+START_TEST (popTest)
+{
+  stacky *stack = NULL;
+
+  stackyPush (stack, 15);
+  stackyPush (stack, 10);
+  stackyPush (stack, 5);
+
+  ck_assert (stackyPop (stack, int) == 5);
+  ck_assert (stackyPop (stack, int) == 10);
+  ck_assert (stackyPop (stack, int) == 15);
+
+  stackyDestroyStack (stack);
+}
+END_TEST
+
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:42:05 2021
+ * Function Name -- peakTest 
+ * Function Purpose -- Tests the functionality of the Stacky module to peak at the element on the top of the stack without removing it.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
+START_TEST (peakTest)
+{
+  stacky *stack = NULL;
+
+  stackyPush (stack, 15);
+  ck_assert (stackyPeak (stack, int) == 15);
+  stackyPush (stack, 10); 
+  ck_assert (stackyPeak (stack, int) == 10);
+  stackyPush (stack, 5);
+  ck_assert (stackyPeak (stack, int) == 5);
+
+  stackyDestroyStack (stack);
+}
+END_TEST
+
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:34:03 2021
+ * Function Name -- stackyCharTest 
+ * Function Purpose -- Tests the functionality of the Stacky stack to handle char data types. Asserting that chars entering the stacky stack are stored in the expected order.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (stackyCharTest)
 {
   stacky *stack = NULL;
@@ -54,6 +130,15 @@ START_TEST (stackyCharTest)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:34:03 2021
+ * Function Name -- stackyShortTest 
+ * Function Purpose -- Tests the functionality of the Stacky stack to handle short data types. Asserting that shorts entering the stacky stack are stored in the expected order.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (stackyShortTest)
 {
   stacky *stack = NULL;
@@ -74,6 +159,15 @@ START_TEST (stackyShortTest)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:34:03 2021
+ * Function Name -- stackyIntTest 
+ * Function Purpose -- Tests the functionality of the Stacky stack to handle int data types. Asserting that ints entering the stacky stack are stored in the expected order.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (stackyIntTest)
 {
   stacky *stack = NULL;
@@ -94,6 +188,15 @@ START_TEST (stackyIntTest)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:34:03 2021
+ * Function Name -- stackyLognTest 
+ * Function Purpose -- Tests the functionality of the Stacky stack to handle logn data types. Asserting that logns entering the stacky stack are stored in the expected order.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (stackyLongTest)
 {
   stacky *stack = NULL;
@@ -115,6 +218,15 @@ START_TEST (stackyLongTest)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:34:03 2021
+ * Function Name -- stackyPtrTest 
+ * Function Purpose -- Tests the functionality of the Stacky stack to handle ptr data types. Asserting that ptrs entering the stacky stack are stored in the expected order.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (stackyPtrTest)
 {
   stacky *stack = NULL;
@@ -135,10 +247,21 @@ START_TEST (stackyPtrTest)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:36:18 2021
+ * Function Name -- isEmptyTestEmptyStack 
+ * Function Purpose -- Tests the 'isEmpty' function to ensure that it properly works on an empty stack.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (isEmptyTestEmptyStack)
 {
   stacky *stack = NULL;
 
+  ck_assert (stackyIsEmpty (stack));
+  
   stackyPush (stack, 10);
   stackyPush (stack, 15);
 
@@ -151,6 +274,15 @@ START_TEST (isEmptyTestEmptyStack)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:39:30 2021
+ * Function Name -- isEmptyTestSingleElement 
+ * Function Purpose -- Tests the 'isEmpty' funciton to ensure that it properly detects a non-empty stack with one element.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (isEmptyTestSingleElement)
 {
   stacky *stack = NULL;
@@ -163,6 +295,15 @@ START_TEST (isEmptyTestSingleElement)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:40:05 2021
+ * Function Name -- isEmptyTestMultipleElements 
+ * Function Purpose -- Tests the 'isEmpty' function to ensure it properly detects a non-empty stack with more than one element.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (isEmptyTestMultipleElements)
 {
   stacky *stack = NULL;
@@ -177,35 +318,4 @@ START_TEST (isEmptyTestMultipleElements)
 }
 END_TEST
   
-START_TEST (popTest)
-{
-  stacky *stack = NULL;
-
-  stackyPush (stack, 15);
-  stackyPush (stack, 10);
-  stackyPush (stack, 5);
-
-  ck_assert (stackyPop (stack, int) == 5);
-  ck_assert (stackyPop (stack, int) == 10);
-  ck_assert (stackyPop (stack, int) == 15);
-
-  stackyDestroyStack (stack);
-}
-END_TEST
-
-START_TEST (peakTest)
-{
-  stacky *stack = NULL;
-
-  stackyPush (stack, 15);
-  ck_assert (stackyPeak (stack, int) == 15);
-  stackyPush (stack, 10); 
-  ck_assert (stackyPeak (stack, int) == 10);
-  stackyPush (stack, 5);
-  ck_assert (stackyPeak (stack, int) == 5);
-
-  stackyDestroyStack (stack);
-}
-END_TEST
-
 #endif
