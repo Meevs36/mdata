10a14c583ab774b7d05c63e6d6c8660622742822
Meevs <levi.meston360@gmail.com>
2021-12-05 20:30:40 +0000

    Added function headers to all tests in the 'linkyBasicFuncTests.h' file. This adds a little bit more description as to what is expected out of the corresponding test.

--- src/test/linky/linkyBasicFuncTests.h
+++ src/test/linky/linkyBasicFuncTests.h
@@ -8,6 +8,15 @@
 #ifndef __LINKY_BASIC_FUNCTIONALITY_TESTS__
 #define __LINKY_BASIC_FUNCTIONALITY_TESTS__
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:25:47 2021
+ * Function Name -- linkyListCreation 
+ * Function Purpose -- Tests the basic creation of a Linky list, asserting that the data it is to be created with is indeed stored correctly and the size of the list is properly initialized.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (linkyListCreation)
 {
   linky *list = NULL;
@@ -21,6 +30,15 @@ START_TEST (linkyListCreation)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:26:28 2021
+ * Function Name -- linkyGetDataTest 
+ * Function Purpose -- Tests the functionality of the 'linkyGetData' macro, asserting that the macro properly extracts the data from the Linky list.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (linkyGetDataTest)
 {
   linky *list = NULL;
@@ -33,6 +51,15 @@ START_TEST (linkyGetDataTest)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:27:03 2021
+ * Function Name -- basicCharList 
+ * Function Purpose -- Tests the compatability of Linky lists with the char data type, asserting that chars within the Linky list are properly stored as they would be expected to be.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (basicCharList)
 {
   linky *charList = NULL;
@@ -51,6 +78,15 @@ START_TEST (basicCharList)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:27:03 2021
+ * Function Name -- basicShortList 
+ * Function Purpose -- Tests the compatability of Linky lists with the short data type, asserting that shorts within the Linky list are properly stored as they would be expected to be.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (basicShortList)
 {
   linky *shortList = NULL;
@@ -67,6 +103,15 @@ START_TEST (basicShortList)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:27:03 2021
+ * Function Name -- basicIntList 
+ * Function Purpose -- Tests the compatability of Linky lists with the int data type, asserting that ints within the Linky list are properly stored as they would be expected to be.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (basicIntList)
 {
   linky *intList = NULL;
@@ -83,6 +128,15 @@ START_TEST (basicIntList)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:27:03 2021
+ * Function Name -- basicLongList 
+ * Function Purpose -- Tests the compatability of Linky lists with the long data type, asserting that longs within the Linky list are properly stored as they would be expected to be.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (basicLongList)
 {
   linky *longList = NULL;
@@ -99,6 +153,15 @@ START_TEST (basicLongList)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:27:03 2021
+ * Function Name -- basicPtrList 
+ * Function Purpose -- Tests the compatability of Linky lists with the ptr data type, asserting that ptrs within the Linky list are properly stored as they would be expected to be.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ */
 START_TEST (basicPtrList)
 {
   char *str1 = "Hello";
@@ -119,6 +182,16 @@ START_TEST (basicPtrList)
 }
 END_TEST
 
+/*
+ * Author -- Meevs
+ * Creation Date -- Sun Dec  5 20:29:28 2021
+ * Function Name -- removeElementTest 
+ * Function Purpose -- Tests the capability of removing elements from the given Linky list, asserting that the remaining elements of the list are properly strung together after the desired element has been removed.
+ * Function Parameters -- 
+ * Function Returns -- void
+ * Notes --
+ *     -- 2021-12-05 -- This test is currently a stub as there is no function to remove elements from a Linky list yet!
+ */
 START_TEST (removeElementTest)
 {
   linky *list = NULL;
